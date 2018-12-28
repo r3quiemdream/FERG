20623c504640f6ccd239edbaea690b76fe270ad7
Fernando <123cookies4life345@gmail.com>
2018-12-27 18:45:22 -0800

    fixing index.html page

--- routes.py
+++ routes.py
@@ -19,5 +19,5 @@ def projects():
 def blog():
     return render_template("blog.html")
 
-# if __name__ == "__main__":
-    # app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
